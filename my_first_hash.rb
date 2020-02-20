43f2bd36b1a09460c5f5c77acf9078437a7426ac
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-20 15:24:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   your_hash = {"name" => "jj", "age" => "33"}# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-  
+
 end
 
 
